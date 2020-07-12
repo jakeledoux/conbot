1d08afc0d62f9300dcdae4ed4b913cc2258cf4f9
Jake Ledoux <contactjakeledoux@gmail.com>
2020-07-12 15:57:46 -0400

    documentation is good

--- conbot.py
+++ conbot.py
@@ -13,14 +13,21 @@
 """
 from fuzzywuzzy import process as fwproc
 import random
-from typing import Dict
+from typing import Dict, Tuple, Union
 
 
 class ConBot(object):
     """ Handles bot personality and responses.
+
+        :param personality_file: Filename of TSV Question/Answer file.
+        :param fallback_messages: Tuple of messages to choose from and
+            return when a response doesn't meet the confidence threshold.
     """
 
-    def __init__(self, personality_file, fallback_messages=None):
+    def __init__(self, personality_file: str,
+                 fallback_messages: Union[Tuple[str, ...], None] = None):
+        """ Constructor method
+        """
         self.dataset = ConBot.load_personality(personality_file)
         if fallback_messages:
             self.fallback_messages = fallback_messages
@@ -32,6 +39,14 @@ class ConBot(object):
     def get_response(self, query: str, threshold=90, debug=False) -> str:
         """ Returns a message in response to `query` using the personality
             dataset loaded.
+
+            :param query: The message for the bot to respond to.
+            :param threshold: The confidence value in percents (0-100) that a
+                response must meet or exceed in order to be displayed.
+            :param debug: If True, the best match and its confidence value will
+                be printed in addition to normal operation.
+
+            :returns: The response string.
         """
         selection, confidence = fwproc.extractOne(query, self.dataset.keys())
         if debug:
@@ -43,7 +58,11 @@ class ConBot(object):
 
     @staticmethod
     def load_personality(filename: str) -> Dict[str, str]:
-        """ Load .tsv conversation dataset as dict.
+        """ Parse .tsv conversation dataset into dictionary.
+
+            :param filename: Filename of TSV Question/Answer file.
+
+            :returns: Dictionary with {Question: Answer} structure.
         """
         with open(filename, 'r', encoding='utf-8') as f:
             contents = f.read()
@@ -59,6 +78,8 @@ class ConBot(object):
 
 
 if __name__ == '__main__':
+    """ A little interactive demo with one of the default personalities.
+    """
     import os
 
     kayla = ConBot(os.path.join('personalities', 'qna_chitchat_friendly.tsv'))
